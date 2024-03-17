4ceeabe22b33a5f0c493a211e17e8f321420bcea
Padraic Reilly <patrick.reilly22@gmail.com>
2024-03-17 15:11:51 -0700

    imported utility 'send-request.js' from my existing fullstack projects

---
+++ src/utilities/send-request.js
@@ -0,0 +1,16 @@
+export default async function sendRequest(url, method = 'GET', payload=null, headers = {}){
+    const options = { method, headers };
+    const res = await fetch(url, options);
+    
+    if (res.ok){
+        const contentType = res.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+            return res.json();
+        } else {
+            return res.text();
+        }
+    } else {
+        const errorData = await res.json();
+        return {error: errorData.error};
+    }
+}
