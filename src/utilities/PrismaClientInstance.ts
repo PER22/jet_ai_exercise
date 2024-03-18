ae59d220d3e0ae0b8797377cbab303ff70b7533b
Padraic Reilly <patrick.reilly22@gmail.com>
2024-03-18 14:44:41 -0700

    ensuring no duplicates of prisma

--- src/utilities/PrismaClientInstance.ts
+++ src/utilities/PrismaClientInstance.ts
@@ -1,6 +1,7 @@
 // PrismaClientInstance.ts
 import { PrismaClient } from '@prisma/client'; 
-const prisma = new PrismaClient();
+let prisma;
+if(!prisma){
+    const prisma = new PrismaClient();
+}
 export default prisma;
-//I am getting this warning: 'This is the 10th instance of Prisma Client being started. Make sure this is intentional.'
-//I'll need to figure it out later, feeling time crunch
