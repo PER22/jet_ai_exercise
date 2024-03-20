28c01f5e42f868b30516c8a031d509ba9677ad12
Padraic Reilly <patrick.reilly22@gmail.com>
2024-03-20 15:11:59 -0700

    added hovering visual styles

--- src/components/AvailableJetsTable/AvailableJetsTable.tsx
+++ src/components/AvailableJetsTable/AvailableJetsTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Jet from '@/interfaces/Jet';
 
 interface AvailableJetsTableProps {
@@ -7,8 +7,16 @@ interface AvailableJetsTableProps {
 }
 
 const AvailableJetsTable: React.FC<AvailableJetsTableProps> = ({ jets, setSelectedJets }) => {
+    const [hoveredJetId, setHoveredJetId] = useState<number | null>(null);
 
-    
+    const handleMouseEnter = (jetId: number) => {
+        setHoveredJetId(jetId);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredJetId(null);
+    };
+
     const handleCheckboxChange = (jetId: number, isChecked: boolean) => {
         setSelectedJets(prevSelectedJets => {
             if (isChecked) {
@@ -19,54 +27,57 @@ const AvailableJetsTable: React.FC<AvailableJetsTableProps> = ({ jets, setSelect
         });
     };
 
-
-
     const toggleCheckbox = (e: React.MouseEvent<HTMLTableCellElement, MouseEvent>, jetId: number) => {
-    if (e.target === document.getElementById(`checkbox-${jetId}`)) {
-        // If yes, do nothing as the checkbox's onChange event will handle it
-        return;
-    }
+        if (e.target === document.getElementById(`checkbox-${jetId}`)) {
+            return;
+        }
 
-    // If the click is not directly on the checkbox, toggle the checkbox's checked state
-    const checkbox = document.getElementById(`checkbox-${jetId}`) as HTMLInputElement;
-    if (checkbox) {
-        checkbox.checked = !checkbox.checked;
-        handleCheckboxChange(jetId, checkbox.checked);
-    }
-};
+        const checkbox = document.getElementById(`checkbox-${jetId}`) as HTMLInputElement;
+        if (checkbox) {
+            checkbox.checked = !checkbox.checked;
+            handleCheckboxChange(jetId, checkbox.checked);
+        }
+    };
 
-    return (<div id="jets-table">
-        <h1>Top 10 Charter Jets</h1>
-        <table>
-            <thead>
-                <tr>
-                    <th>Select</th>
-                    <th>Name</th>
-                    <th>Wingspan (ft)</th>
-                    <th>Engines</th>
-                    <th>Manufactured</th>
-                </tr>
-            </thead>
-            <tbody id="jet-table-body">
-                {jets && jets.map(jet => (
-                    <tr key={jet.id} onClick={(e) => toggleCheckbox(e, jet.id)}>
-                    <td >
-                        <input 
-                            type="checkbox" 
-                            name={`checkbox-${jet.id}`} 
-                            id={`checkbox-${jet.id}`} 
-                            onChange={(e) => handleCheckboxChange(jet.id, e.target.checked)} 
-                        /> 
-                    </td>
-                    <td>{jet.name}</td>
-                    <td>{jet.wingspan}</td>
-                    <td>{jet.engines}</td>
-                    <td>{jet.year}</td>
-                </tr>))
-                    }
-            </tbody>
-        </table>
-    </div>
+    return (
+        <div id="jets-table">
+            <h1>Top 10 Charter Jets</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Select</th>
+                        <th>Name</th>
+                        <th>Wingspan (ft)</th>
+                        <th>Engines</th>
+                        <th>Manufactured</th>
+                    </tr>
+                </thead>
+                <tbody id="jet-table-body">
+                    {jets && jets.map(jet => (
+                        <tr 
+                            key={jet.id} 
+                            onClick={(e) => toggleCheckbox(e, jet.id)}
+                            onMouseEnter={() => handleMouseEnter(jet.id)}
+                            onMouseLeave={handleMouseLeave}
+                            style={{ backgroundColor: hoveredJetId === jet.id ? '#f2f2f270' : 'transparent' }}
+                        >
+                            <td>
+                                <input 
+                                    type="checkbox" 
+                                    name={`checkbox-${jet.id}`} 
+                                    id={`checkbox-${jet.id}`} 
+                                    onChange={(e) => handleCheckboxChange(jet.id, e.target.checked)} 
+                                />
+                            </td>
+                            <td>{jet.name}</td>
+                            <td>{jet.wingspan}</td>
+                            <td>{jet.engines}</td>
+                            <td>{jet.year}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     );
 };
 
